a2406fe9b7a51f629cc22b6e29dc36968b275536
BrandoWeaver <srunrotha614@gmail.com>
2025-01-28 12:05:49 +0700

    remove ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  ssr: false,
   postcss: {
     plugins: {
       tailwindcss: {},
