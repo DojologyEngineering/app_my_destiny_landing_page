3b4484aa61965d630db4ae2db483db9512e247f3
BrandoWeaver <srunrotha614@gmail.com>
2025-01-28 23:08:14 +0700

    add head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,4 +49,63 @@ export default defineNuxtConfig({
       routes: ['/'],
     },
   },
+  app: {
+    head: {
+      title: 'Appmydestiny - Team Feng Shui App of Cambodia',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Responsible for the international level of 600,000 students.',
+        },
+        // Open Graph (Facebook, LinkedIn, etc.)
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Appmydestiny - Team Feng Shui App of Cambodia',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Responsible for the international level of 600,000 students.',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://appmydestiny.com/kh/logo.png',
+        }, // Replace with your logo URL
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://appmydestiny.com/kh',
+        }, // Replace with your website URL
+        // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: 'Appmydestiny - Team Feng Shui App of Cambodia',
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content:
+            'Responsible for the international level of 600,000 students.',
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: 'https://appmydestiny.com/kh/logo.png',
+        }, // Replace with your logo URL
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        }, // Use 'summary_large_image' for large previews
+      ],
+    },
+  },
 });
